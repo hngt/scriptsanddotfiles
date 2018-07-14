07dfe1e3145dbca289796e92605cf114eb0a007e
redacted <redacted@example.com>
2018-07-14 07:59:45 +0200

    updating stuff as time passes on and I change my opinions

--- dotfiles/.Xsetroot.sh
+++ dotfiles/.Xsetroot.sh
@@ -1,4 +1,3 @@
-DISPLAY=:0
 #SOUND=`XDG_RUNTIME_DIR=/tmp/xdg-runtime-lich DISPLAY=:0 pamixer --get-volume`
 SOUND=`amixer get Master | awk '$0~/%/{print $4}' | tr -d '[]'`
 LOAD=`uptime|grep -o '[0-9]\+\.[0-9][0-9]'|awk  -vRS= -vFS="\n" '{print  $1" "$2" "$3}'`
@@ -7,6 +6,22 @@ RAM_USAGE=`free | grep Mem | awk '{printf("%.2f%", $3/$2 * 100.0)}'`
 DATE=`date +'%a %d %b %H:%M'`
 #MPD=`mpc current|head -c40`
 MOC=`mocp -Q "%artist - %song"|head -c40`
-echo ${ACPI}
+UMSG=$(find $HOME/.mutt/mailbox/uni/inbox/new -type f | wc -l);
+[ "$UMSG" != "0" ] && EMAILS_UNI="U: $UMSG | "
+PMSG=$(find $HOME/.mutt/mailbox/personal/inbox/new -type f | wc -l);
+[ "$PMSG" != "0" ] && EMAILS_PERSONAL="P: $PMSG | "
+CMSG=$(find $HOME/.mutt/mailbox/cock/inbox/new -type f | wc -l);
+[ "$CMSG" != "0" ] && EMAILS_COCK="C: $CMSG | "
+# battery check
+#if [ "$ACPI" = '15%' ]; then
+#    aplay /home/lich/music/samples/synth/synth-nattegeit.wav
+#elif [ "$ACPI" = '10%' ]; then
+#    aplay /home/lich/music/samples/moje/micheljordan.wav
+#elif [ "$ACPI" = '7%' ]; then
+#    sct 1000
+#elif [ "$ACPI" = '5%' ]; then
+#    sudo ZZZ
+#fi
+
 #xsetroot -name "${MPD} | ${LOAD} | ${RAM_USAGE} | ${SOUND}% | VPN:${VPN} | ${ACPI} | ${DATE}"
-xsetroot -name "${MOC} | ${LOAD} | R:${RAM_USAGE} | V:${SOUND} | B:${ACPI} | ${DATE}"
+xsetroot -name "${MOC} | ${EMAILS_UNI}${EMAILS_PERSONAL}${EMAILS_COCK}${LOAD} | R:${RAM_USAGE} | V:${SOUND} | B:${ACPI} | ${DATE}"
