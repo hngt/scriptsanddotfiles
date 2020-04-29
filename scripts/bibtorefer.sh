07ffdb6ab805825f72c2376e7faec61a9054b937
redacted <redacted@example.com>
2020-04-29 12:09:20 +0200

    lockdown upd8

--- scripts/bibtorefer.sh
+++ scripts/bibtorefer.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 sed "
+	s/@\(book\|article\|InCollection\)\s*{\(.*\),/%L \2/I
 	s/title\s*=\s*{\(.*\)},*/%T \1/I
 	s/author\s*=\s*{\(.*\)},*/%A \1/I
 	s/publisher\s*=\s*{\(.*\)},*/%I \1/I
+	s/editor\s*=\s*{\(.*\)},*/%E \1/I
 	s/doi\s*=\s*{\(.*\)},*/%K \1/I
 	s/journal\s*=\s*{\(.*\)},*/%J \1/I
 	s/volume\s*=\s*{\(.*\)},*/%V \1/I
 	s/number\s*=\s*{\(.*\)},*/%N \1/I
 	s/pages\s*=\s*{\(.*\)},*/%P \1/I
+	s/booktitle\s*=\s*{\(.*\)},*/%B \1/I
+	s/howpublished\s*=\s*{.url{\(.*\)}},/%O URL: \1 (accessed $(date '+%e %B %Y'))/I
 	s/--/-/I
-	s/year\s*=\s*\([0-9]*\),*/%D \1/I
+	s/%A \([A-Z.a-z]*\), \([A-Z .a-z]*\)/%A \2 \1/I
+	s/year\s*=\s*{\(.*\)},*/%D \1/I
 	s/^\s*//g
 	/^[^%]/d
 	s/\({\|}\)//g
