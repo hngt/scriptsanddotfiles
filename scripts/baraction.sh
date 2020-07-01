2fb422ec51341a476ec0fb46e21bc72c7d49c381
lich-tex <lich@dataswamp.org>
2020-07-01 08:24:02 +0200

    cleanup and adding content-viewer

--- scripts/baraction.sh
+++ scripts/baraction.sh
@@ -3,16 +3,6 @@ panelfifo=/tmp/panel
 rm -f $panelfifo
 mkfifo $panelfifo
 
-corona(){
-	while true; do
-		coronafile="$HOME/.cache/corona"
-		[ "$(stat --format="%y" "$coronafile" 2>/dev/null | cut -d ' ' -f1)" != "$(date +%F)" ] && 
-			curl -s "https://coronavirus-tracker-api.herokuapp.com/v2/locations?country_code=PL" > "$coronafile"
-		jq -j '.latest|"cC:",.confirmed," D:",.deaths,"\n"' ~/.cache/corona
-		sleep 3600
-	done
-}
-
 sound(){
 	while true; do
 		volume="$(amixer get Master | grep -Eo '[0-9]{1,3}%')"
@@ -53,12 +43,12 @@ memory(){
 curdate(){
 	while true; do
 		date "+D%A %d.%m.%y %H:%M"
-		sleep 2
+		sleep 60
 	done
 }
 
 mails(){
-	while sleep 2; do
+	while sleep 30; do
 		emails=""
 		for i; do
 			mailcount="$(ls -1 "$HOME/.mutt/mailbox/$i/inbox/new" | wc -l)"
@@ -72,9 +62,6 @@ bargen(){
 	pad=" | "
 	while read -r line; do
 		case $line in
-			c*) corona="${line#?}";;
-			C0) countdown="";;
-			C*) countdown="${line#?}$pad";;
 			B*) battery="B:${line#?}";;
 			D*) cal="${line#?}";;
 			L*) load="${line#?}";;
@@ -84,7 +71,7 @@ bargen(){
 			V*) volume="V:${line#?}";;
 			X*) mus="${line#?}";;
 			esac
-	echo "${mus}${pad}${mails}${countdown}${load}${pad}${corona}${pad}${ram}${pad}${volume}${pad}${battery}${pad}${cal}"
+	echo "${mus}${pad}${mails}${load}${pad}${ram}${pad}${volume}${pad}${battery}${pad}${cal}"
 	done
 }
 corona > $panelfifo &
@@ -93,6 +80,6 @@ load_avg > $panelfifo &
 bttry > $panelfifo &
 memory > $panelfifo &
 curdate > $panelfifo &
-mails uni personal dataswamp cock > $panelfifo &
+mails uni personal dataswamp > $panelfifo &
 
 bargen < $panelfifo
